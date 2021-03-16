daff9f399ab7a866a44a2aa732dee12b65b0195e
Magnus Bärnholt <magnus.barnholt@me.com>
2021-03-16 14:30:56 +0100

    Notes App

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_FRCUZ6hg8",
     "aws_user_pools_web_client_id": "1271kplhat7j2a66q4euosjab8",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://pzy7n322anfkzlqek7gj2iqthq.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-juofddlxjrasjamsmkdlq5vjxm"
 };
 
 
